6cfb88b6d4dea38be2cffc9d557334410368f134
Faricefaridah <faricefaridah@gmail.com>
2026-06-08 02:10:12 +0300

    clean build for vercel

--- docs/server/assets/_tanstack-start-manifest_v-CE36whdT.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/ADMN/Desktop/FARICE/PEMI/PEMI NEW/image-shimmer-showcase/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-Ce34b0sP.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-Ce34b0sP.js" } }] }, "/": { filePath: "C:/Users/ADMN/Desktop/FARICE/PEMI/PEMI NEW/image-shimmer-showcase/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-hWfdhPG3.js"] } } });
-export {
-  tsrStartManifest
-};
